5adcc82a738ed0a6caf945113f68321f413bf2a0
iOS Apps Development <info@iosappsdev.org>
2014-08-05 20:41:52 -0700

    Initial Commit

---
+++ spriteKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  spriteKitDemo
+//
+//  Created by Ricardo Ruiz on 8/5/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spriteKitDemo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  spriteKitDemo
+//
+
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
